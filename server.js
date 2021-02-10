90bc91fe998d20bb2a94e99762ac8edbf56340ee
stefanjepsen <43956167+stefanjepsen@users.noreply.github.com>
2021-02-10 10:58:14 +0100

    startup RESTAPI-Project

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+require("dotenv-flow").config();
+
+// Prints message when server starts
+app.listen(() => console.log('It has begun...'))
+
+//Routes
+app.get("/api/welcome", (req, res) => {
+    
+    res.status(200).send({message: "This is an API Call !"});
+    
+
+});
+
+
+
+mongoose.connect
+(
+    process.env.DBHOST,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+).catch(error => console.log("Error connecting to MOngoDB:" + error));
+
+mongoose.connection.once("open", () => console.log("Connected succefully to MongoDB"));
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, function() {
+    console.log("Server is running on port: " + PORT) ;
+})
+
+module.exports = app;
